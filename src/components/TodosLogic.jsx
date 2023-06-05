ada0411b807a0c09c4fafb62d1ed0a7890bd88d2
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-05 21:30:10 +0300

    Generate unique id for each todo

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import InputTodo from "@/components/InputTodo";
 import TodosList from "@/components/TodosList"
+import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
     const [todos, setTodos] = useState([
         {
-            id: 1,
+            id: uuidv4(),
             title: 'Setup development environment',
             completed: true,
           },
           {
-            id: 2,
+            id: uuidv4(),
             title: 'Develop website and add content',
             completed: false,
           },
           {
-            id: 3,
+            id: uuidv4(),
             title: 'Deploy to live server',
             completed: false,
           },
@@ -24,7 +25,7 @@ const TodosLogic = () => {
     const addTodoItem = (title) => {
         console.log(title);
         const newTodo = {
-            id:4,
+            id: uuidv4(),
             title: title,
             completed: false
         }
