1993659dcf0f945c72dfca24c797bd8a981cf2f3
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-05 21:42:55 +0300

    Preventing Empty submission

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,24 +1,36 @@
 import { useState } from "react";
 
-const InputTodo = ( { addTodoItem }) => {
+const InputTodo = ({ addTodoItem }) => {
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
 
-    const [title, setTitle] = useState('');
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        addTodoItem(title);
-        setTitle('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle("");
+      setMessage("");
+    } else {
+      setMessage("Please add an item");
     }
-    const handleInput = (e) => {
-        setTitle(e.target.value)
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Write her" value={title} onChange={handleInput}/>
-            <button>Submit</button>
-        </form>
-    )
-}
+  };
+  const handleInput = (e) => {
+    setTitle(e.target.value);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Write her"
+          value={title}
+          onChange={handleInput}
+        />
+        <button>Submit</button>
+      </form>
+      <span>{message}</span>
+    </>
+  );
+};
 
 export default InputTodo;
