8be9d8b56fb8c1927ebb82d4f5fa03616ceed6ee
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-05 07:37:42 +0300

    Created Todos Components

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,10 @@
+const TodoApp = () => {
+    return (
+        <div>
+            <h1>This is my Todo Application</h1>
+            <p>My todo items</p>
+        </div>
+    )
+}
+
+export default TodoApp;
