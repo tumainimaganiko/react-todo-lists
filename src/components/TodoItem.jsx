1df42b42e81d70be26117abeeb56d050aaf911bb
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-06 07:11:10 +0300

    Add Dynamic styling

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,22 @@
 import styles from "@/styles/TodoItem.module.css"
 
 const TodoItem = ({ itemList, handleChange, delTodo }) => {
+
+    const completedStyle = {
+        fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+    }
     
     return (
         <li className={styles.item}>
             <div className={styles.content}>
             <input type="checkbox" checked={itemList.completed} onChange={() => handleChange(itemList.id)}/>
             <button onClick={() => delTodo(itemList.id)}>Delete</button>
+            <span style={itemList.completed ? completedStyle : null}>
             {itemList.title}
+            </span>
             </div>
         </li>
     )
