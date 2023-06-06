bf095ecf11805603cdf70a268684a9ecc9ba391a
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-06 11:59:26 +0300

    Enable User editing

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,48 +1,62 @@
-import { useState } from "react"
-import styles from "@/styles/TodoItem.module.css"
+import { useState } from "react";
+import styles from "@/styles/TodoItem.module.css";
 
 const TodoItem = ({ itemList, handleChange, delTodo, setUpdate }) => {
-    const [edit, setEdit] = useState(false);
+  const [edit, setEdit] = useState(false);
+  //   const editInputRef = useRef(null)
+  const [updateInput, setUpdateInput] = useState(itemList.title);
 
-    let viewMode = {};
-    let editMode = {};
-    if(edit){
-        viewMode.display = 'none';
-    }else {
-        editMode.display = 'none';
-    }
-    const handleEdit = () => {
-        setEdit(true);
-    }
+  let viewMode = {};
+  let editMode = {};
+  if (edit) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+  const handleEdit = () => {
+    setEdit(true);
+  };
 
-    const handleUpdatedDone = (event) => {
-        if(event.key === 'Enter'){
-            setEdit(false);
-        }
+  const handleUpdatedDone = (event) => {
+    if (event.key === "Enter") {
+      setUpdate(updateInput, itemList.id);
+      setEdit(false);
     }
+  };
 
-    const completedStyle = {
-        fontStyle: 'italic',
-    color: '#595959',
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
     opacity: 0.4,
-    textDecoration: 'line-through',
-    }
-    
-    return (
-        <li className={styles.item}>
-            <div className={styles.content} style={viewMode}>
-            <input type="checkbox" checked={itemList.completed} onChange={() => handleChange(itemList.id)}/>
-            <button onClick={handleEdit}>Edit</button>
-            <button onClick={() => delTodo(itemList.id)}>Delete</button>
-            <span style={itemList.completed ? completedStyle : null}>
-            {itemList.title}
-            </span>
-            </div>
-            <input type="text" value={itemList.title} className={styles.textInput} style={editMode} 
-                onChange={(e) => setUpdate(e.target.value, itemList.id)} onKeyDown={handleUpdatedDone}
-            />
-        </li>
-    )
-}
+    textDecoration: "line-through",
+  };
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemList.completed}
+          onChange={() => handleChange(itemList.id)}
+        />
+        <button onClick={handleEdit}>Edit</button>
+        <button onClick={() => delTodo(itemList.id)}>Delete</button>
+        <span style={itemList.completed ? completedStyle : null}>
+          {updateInput}
+        </span>
+      </div>
+      <input
+        type="text"
+        // ref={editInputRef}
+        // defaultValue={itemList.title}
+        value={updateInput}
+        className={styles.textInput}
+        style={editMode}
+        onChange={(e) => setUpdateInput(e.target.value)}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
 
 export default TodoItem;
